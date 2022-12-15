3b3829b58eb9cad9b749427f86d365a2e3ce3d1e
Tona <tona@mango-byte.com>
2022-12-15 16:14:13 +0700

    add

--- src/operators/spread.js
+++ src/operators/spread.js
@@ -7,3 +7,43 @@ console.log(tahoe.join(", ")); // Tallac, Ralston, Rose, Ward, Blackwood
 
 peaks.reverse();
 console.log(peaks.join(", ")); // Rose, Ralston, Tallac
+
+const lakes = ["Donner", "Marlette", "Fallen Leaf", "Cascade"];
+
+const [first, ...others] = lakes;
+
+console.log(others.join(", ")); // Marlette, Fallen Leaf, Cascade
+
+
+function directions(...args) {
+    let [start, ...remaining] = args;
+    let [finish, ...stops] = remaining.reverse();
+
+    console.log(`drive through ${args.length} towns`);
+    console.log(`start in ${start}`);
+    console.log(`the destination is ${finish}`);
+    console.log(`stopping ${stops.length} times in between`);
+}
+
+directions("Truckee", "Tahoe City", "Sunnyside", "Homewood", "Tahoma");
+
+
+const morning = {
+    breakfast: "oatmeal",
+    lunch: "peanut butter and jelly"
+};
+
+const dinner = "mac and cheese";
+
+const backpackingMeals = {
+    ...morning,
+    dinner
+};
+
+console.log(backpackingMeals);
+
+// {
+//   breakfast: "oatmeal",
+//   lunch: "peanut butter and jelly",
+//   dinner: "mac and cheese"
+// }
---
+++ src/synchronous/index.js
@@ -0,0 +1,3 @@
+/*import fetch from 'node-fetch';
+
+console.log(fetch("https://api.randomuser.me/?nat=US&results=1"));*/
